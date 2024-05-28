fa0d09ea9d6e21b7cf0252a2fb0bb28cb5592c84
Omegastar19 <117172527+Omegastar19@users.noreply.github.com>
2024-05-28 12:13:57 +0200

    Update BarcodeScanner.py

--- BarcodeScanner.py
+++ BarcodeScanner.py
@@ -1,6 +1,9 @@
+import time
+import cv2
+import zxingcpp
+
 class BarcodeScanner:
     def __init__(self, database, capture_device=0, delay=1):
-        # The delay value specifies how long the camera waits between each scan
         self.database = database
         self.capture_device = capture_device
         self.delay = delay  # Delay in seconds between scans
@@ -21,12 +24,6 @@ class BarcodeScanner:
                       f'\n Format:   {result.format}'
                       f'\n Content:  {result.content_type}'
                       f'\n Position: {result.position}')
-                write_barcode_text_to_file(result.text)
-                matched_product = match_barcode_with_database(result.text, self.database)
-                if matched_product:
-                    print(f'Matched Product: {matched_product}')
-                else:
-                    print('No match found in the database.')
         else:
             print("Could not find any barcode.")
 
@@ -54,5 +51,6 @@ class BarcodeScanner:
         cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    scanner = BarcodeScanner(barcode_database)
+    database = {}  # Placeholder for your database
+    scanner = BarcodeScanner(database)
     scanner.run()
